2ad60ddc6b6a09bc5b337a387d116605f8025493
akash kinkar pandey <akashkinkarpandey@gmail.com>
2022-07-15 19:44:00 +0530

    volume.java updated

--- Java-oops/02/Volume.java
+++ Java-oops/02/Volume.java
@@ -1,5 +1,26 @@
 import java.util.*;
+//Qs
+//Create a Box class and create method setData() to initialize members
+//Write another method to return volume
+//create a cuboid and cube ,find which one is bigger. 
 
+//input-output
+// Enter length,breadth,height of cuboid
+// 4 5 6
+// Enter side of cube
+// 5
+// Volume of Cuboid is 120.0
+// Volume of Cube is 125.0
+// Cube has greater volume
+
+//input-output
+// Enter length,breadth,height of cuboid
+// 1 1 1
+// Enter side of cube
+// 1
+// Volume of Cuboid is 1.0
+// Volume of Cube is 1.0
+// Cube and cuboid have same volume
 class Box {
 	double length, breadth, height;
 
@@ -33,8 +54,10 @@ public class Volume {
 		System.out.println("Volume of Cube is " + second);
 		if (first > second) {
 			System.out.println("Cuboid has greater volume");
-		} else {
+		} else if (first < second) {
 			System.out.println("Cube has greater volume");
+		} else {
+			System.out.println("Cube and cuboid have same volume");
 		}
 		sc.close();
 	}
