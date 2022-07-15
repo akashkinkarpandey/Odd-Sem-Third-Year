97e816851d2f1a5b603270fe98070a9243e49adf
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2022-07-15 16:24:35 +0530

    added just for fun

---
+++ Java-oops/02/Volume.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Box
+{
+	double length,breadth,height;
+	public void setData(double l,double b,double h)
+	{
+	length=l;
+	breadth=b;
+	height=h;
+	}
+	public double findVol()
+	{
+	return length*breadth*height;
+	}
+}
+public class Volume
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+Box rectangle=new Box();
+Box cube=new Box();
+System.out.println("Enter length,breadth,height of cuboid");
+double l=sc.nextDouble();
+double b=sc.nextDouble();
+double h=sc.nextDouble();
+rectangle.setData(l,b,h);
+System.out.println("Enter side of cube");
+double a=sc.nextDouble();
+cube.setData(a,a,a);
+double first=rectangle.findVol();
+double second=cube.findVol();
+System.out.println("Volume of Cuboid is "+first);
+System.out.println("Volume of Cube is "+second);
+if(first>second)
+{
+System.out.println("Cuboid has greater volume");
+}
+else
+{
+System.out.println("Cube has greater volume");
+}
+}
+}
---
+++ Java-oops/02/Volume2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Box2
+{
+	double length,breadth,height;
+	Box2()
+	{
+	}
+	public double findVol()
+	{
+	return length*breadth*height;
+	}
+}
+public class Volume2
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter length,breadth,height of cuboid");
+double l=sc.nextDouble();
+double b=sc.nextDouble();
+double h=sc.nextDouble();
+Box2 rectangle= new Box2();
+rectangle.length=l;
+rectangle.breadth=b;
+rectangle.height=h;
+System.out.println("Enter side of cube");
+double a=sc.nextDouble();
+Box2 cube=new Box2();
+cube.length=a;
+cube.breadth=a;
+cube.height=a;
+double first=rectangle.findVol();
+double second=cube.findVol();
+System.out.println("Volume of Cuboid is "+first);
+System.out.println("Volume of Cube is "+second);
+if(first>second)
+{
+System.out.println("Cuboid has greater volume");
+}
+else
+{
+System.out.println("Cube has greater volume");
+}
+}
+}
