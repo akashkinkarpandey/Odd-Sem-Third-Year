b3f345daac54ffe464db34cf97c1b76ebfd2697c
akash kinkar pandey <akashkinkarpandey@gmail.com>
2022-08-14 09:19:01 +0530

    folder of day 6 java renamed

--- Java-oops/05-/program-2/new.java
+++ Java-oops/05-/program-2/new.java
@@ -1,1 +1,7 @@
-//yet to be added
+// Question
+// Create a class Account with data members Account_Number,Account_Balance
+// add methods to deposit & withdraw money & display balance
+// Derive 2 subclasses Savings_Account and Current_Account where Savings Account should maintain minimum account balance as 1000 and Current Account should have overdraft facility. 
+// In addition,  Savings_Account should have data member interest, method to calculate interest and add to account balance.
+// for Current Account there should be an overdraft limit.
+// Use pertinent methods to display the information
